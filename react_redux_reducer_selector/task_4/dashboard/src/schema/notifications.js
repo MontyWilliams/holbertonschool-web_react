e9495ae6984f57be17b37b1da6eb33a4e891684c
Montywilliams <3650@holbertonschool.com>
2023-10-12 14:43:28 -0600

    init pus

---
+++ react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -0,0 +1,21 @@
+import * as notifications from '../notifications'
+import { schema, normalize } from 'normalizr';
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid',
+});
+
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notifications, [notification]);
+
+export function getAllNotificationsByUser(userId) {
+  return Object.values(normalizedData.entities.notifications)
+    .filter((note) => note.author == userId)
+    .map((note) => normalizedData.entities.messages[note.context]);
+}
